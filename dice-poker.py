f98185abfe6e1b5137077ae53ad26873874c43e8
tum0xa <mail@tum0xa.ru>
2018-08-29 16:57:37 +0500

    Rebase all

--- dice-poker.py
+++ dice-poker.py
@@ -1,48 +1,25 @@
 import random
+import os
 
 NUM_OF_FACES = 6
 NUM_OF_DICES = 5
 
-State = {0:'Nothing',
-        1: 'Pair (1)',
-        2: 'Pair (2)',
-        3: 'Pair (3)',
-        4: 'Pair (4)',
-        5: 'Pair (5)',
-        6: 'Pair (6)',
-        }
-
 NOTHING = 0
+PAIR = 2
+SET = 3
+CARE = 4
+POKER = 5
+STRAIGHT = 100
+FULL_HOUSE = 23
 
-PAIR1 = 1
-PAIR2 = 2
-PAIR3 = 3
-PAIR4 = 4
-PAIR5 = 5
-PAIR6 = 6
 
-TWO_PAIR = 7
-
-SET1 = 10
-SET2 = 11
-SET3 = 12
-SET4 = 13
-SET5 = 14
-SET6 = 15
-
-STRAIGHT = 16
-
-FULL_HOUSE = 17
-
-CARE1 = 18
-CARE2 = 19
-CARE3 = 20
-CARE4 = 21
-CARE5 = 22
-CARE6 = 23
-
-POKER = 24
-
+sym_dices = { 1: '⚀',
+              2: '⚁',  
+              3: '⚂',
+              4: '⚃',
+              5: '⚄',
+              6: '⚅',
+            }
 
 
 class Dice:
@@ -60,7 +37,7 @@ class Dice:
         self.__act_face = random.randint(1,self.__num_faces)
     
     def draw(self):
-        print(self.act_face)
+        print(sym_dices.get(self.act_face),end = ' ')
         
     @property
     def act_face(self):
@@ -84,94 +61,85 @@ class Player:
     def get_dices(self,dices):
         if dices:
             self.__hand = dices
-            print(self.name,'has now', len(self.__hand), 'dices in the hand')
+            # ~ print(self.name,'has now', len(self.__hand), 'dices in the hand')
         else:
-            print('Nothing to getting')
+            # ~ print('Nothing to getting')
+            pass
     
-    def roll_dices(self,dice_to_roll):
+    def get_state(self):
+        return self.__state
+            
+    def set_state(self,state):
+        self.__state = state
         
-        for dice in self.__hand:
-            if self.hand.index(dice) in dices_to_roll:
-                dice.roll()
+
                 
-    def roll_dices(self):
-        self.__state = NOTHING
-        for dice in self.__hand:
-            dice.roll()
-        
-        print(self.name,'rolled the dices')
+    # ~ def roll_dices(self):
+        # ~ self.__state = NOTHING
+        # ~ for dice in self.__hand:
+            # ~ dice.roll()
+    
+    def roll_dices(self,dices_to_roll=None):
+        if dices_to_roll==None:
+            self.__state = NOTHING
+            for dice in self.__hand:
+                dice.roll()
+        else:
+            for dice in self.__hand:
+                if self.__hand.index(dice) in dices_to_roll:
+                    dice.roll()
+        # ~ print(self.name,'rolled the dices')
         
     def show_hand(self):
         dice_to_show = []
         for dice in self.__hand:
             dice_to_show.append(str(dice))
         return dice_to_show
-        
-    def check_hand(self):
-        hand = self.show_hand()
-        pair = 0
-        set_s = False
-        self.__state = NOTHING
-        
-        if hand.count('1') == 2:
-            self.__state += PAIR1
-            pair += 1
-        if hand.count('2') == 2:
-            self.__state += PAIR2
-            pair += 1
-        if hand.count('3') == 2:
-            self.__state += PAIR3
-            pair += 1
-        if hand.count('4') == 2:
-            self.__state += PAIR4
-            pair += 1
-        if hand.count('5') == 2:
-            self.__state += PAIR5
-            pair += 1
-        if hand.count('6') == 2:
-            self.__state += PAIR6
-            pair += 1
-        if hand.count('1') == 3:
-            self.__state += SET1
-            set_s = True
-        elif hand.count('2') == 3:
-            self.__state += SET2
-            set_s = True
-        elif hand.count('3') == 3:
-            self.__state += SET3
-            set_s = True
-        elif hand.count('4') == 3:
-            self.__state += SET4
-        elif hand.count('5') == 3:
-            set_s = True
-            self.__state += SET5
-        elif hand.count('6') == 3:
-            self.__state += SET6
-            set_s = True
             
-        if hand.count('1') == 4:
-            self.__state += CARE1
-        elif hand.count('2') == 4:
-            self.__state += CARE2
-        elif hand.count('3') == 4:
-            self.__state += CARE3
-        elif hand.count('4') == 4:
-            self.__state += CARE4
-        elif hand.count('5') == 4:
-            self.__state += CARE5
-        elif hand.count('6') == 4:
-            self.__state += CARE6  
+    def draw_hand(self):
+        for dice in self.__hand:
+            dice.draw()
+        print('')
         
-        if set_s == True and pair == 1: 
-            self.__state += FULL_HOUSE
+def check_hand(hand):
+    state = {}
+    result = NOTHING
+    
+    for i in range(1,7):
+        state.update({i:hand.count(str(i))})
         
-        if set(hand) == ['1','2','3','4','5'] or set(hand) == ['2','3','4','5','6']:
-            self.__state += STRAIGHT
-            
-        if pair == 2:
-            self.__state += TWO_PAIR
-            
-        return self.__state
+    # ~ print(state)
+    
+    
+    if list(state.values()).count(1) == 5:
+        tmp = []
+        for key in state:
+            if state.get(key) != NOTHING:
+                tmp.append(key)
+        if set(tmp) == {1,2,3,4,5} or set(tmp) == {2,3,4,5,6}:
+            result += STRAIGHT
+    elif POKER in state.values():
+        result += POKER*1000
+    
+    elif CARE in state.values():
+        result += CARE*100
+    
+    elif PAIR in state.values() and SET in state.values():
+        result += FULL_HOUSE*100
+        
+    else:
+        if PAIR in state.values():
+            for key in state:
+                if state.get(key)==PAIR:
+                    result+=key+PAIR
+                    
+        if SET in state.values():
+            for key in state:
+                if state.get(key)==SET:
+                    result+=key+SET*10
+                
+    
+    return result
 
         
 def get_random_dices(num_of_dices,num_of_faces):
@@ -182,22 +150,72 @@ def get_random_dices(num_of_dices,num_of_faces):
     return dices
 
 
-
-
 def main():
-    player1 = Player('Andrey')
-    player2 = Player('Timofey')
+    num_of_players = int(input("Сколько игроков за столом? - " ))
+    players = []
+    first_player = 0
+    for i in range(num_of_players):
+        player_name = input("Как зовут игрока №" + str(i + 1) + "? - ")
+        player = Player(player_name)
+        print(player.name, "садится за стол.")
+        players.append(player)
+        
+    input("Чтобы начать игру нажмите 'Enter'. ")
+        
+    for player in players:
+        player.get_dices(get_random_dices(NUM_OF_DICES,NUM_OF_FACES))
+        print(player.name, "берет кости в руку.")   
     
-    player1.get_dices(get_random_dices(NUM_OF_DICES,NUM_OF_FACES))
-    player2.get_dices(get_random_dices(NUM_OF_DICES,NUM_OF_FACES))
+    try:
+        first_player = int(input("Кто кидает кости первым? Введите число от \
+1 до " + str(num_of_players) + ". Для случайного выбора нажмите 'Enter' без ввода числа. "))
+    except ValueError:
+        first_player = random.randint(1,num_of_players)
+        
+    print(players[first_player-1].name, "первым кидает кости.")
+
+    
+    if first_player != 1:
+        for i in range(num_of_players):
+            player = players[i] 
+            if i < first_player-1:
+                players.append(players[i])
+                players.pop(i)
+    
+    for player in players:
+        input(player.name +"! Чтобы кинуть кости нажми 'Enter'. ")
+        print(player.name, " кидает кости.")
+        player.roll_dices()
+        print("Результат броска: ", end='')
+        player.draw_hand()
+        
+    # round 1
+    dices_to_reroll = []
+    for player in players:
+        dices_to_reroll = input(player.name + " какие кости будешь перебрасывaть. Напиши порядкове номера через запятую: - ")
+        player.roll_dices(dices_to_roll=dices_to_reroll.split(','))
+        print("Результат броска: ", end='')
+        player.draw_hand()
     
     # ~ print(player1.name,'has:', player1.show_hand())
     # ~ print(player2.name,'has:', player2.show_hand())
+
+    # ~ input("Press 'Enter' to roll dice")
     
-    player1.roll_dices()
-    print(player1.name,'has:', player1.show_hand(), "State:",player1.check_hand())
-    player2.roll_dices()
-    print(player2.name,'has:', player2.show_hand(),"State:",player2.check_hand())
+    # ~ while stage != END:
+        # ~ kb = input("Next step?")
+        # ~ if kb == 'q':
+            # ~ break
+        # ~ else:
+            # ~ os.system('clear')
+            # ~ player1.roll_dices()
+            # ~ player1.set_state(check_hand(player1.show_hand()))
+            # ~ print(player1.name,'has:', player1.show_hand(), "State:",player1.get_state())
+            # ~ player2.roll_dices()
+            # ~ player2.set_state(check_hand(player2.show_hand()))
+            # ~ print(player2.name,'has:', player2.show_hand(),"State:",player2.get_state())
+        
+        
     
     #dices_to_roll = input(len(hand),"dice(s) in the hand. Pick dices to reroll. (type dice's number by comma)").split(',')
     
